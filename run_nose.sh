3792f977688f7360e7b4078470f3c1d2fa7252f4
Inkvi <inkvi@yandex-team.ru>
2012-03-16 19:56:29 +0400

    Disable log messaged during tests

--- run_nose.sh
+++ run_nose.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-nosetests -w tests $1 --with-coverage --cover-package=brukva --nologcapture --nocapture -v
+nosetests -w tests $1 --with-coverage --cover-package=brukva --nocapture -v
 
