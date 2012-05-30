8405e8cdeabb7bc439e3222988a566c3a2ac2832
Inkvi <inkvi@yandex-team.ru>
2012-05-30 19:19:01 +0400

    Demos applications are improved

--- demos/bench/app.py
+++ demos/bench/app.py
@@ -1,3 +1,4 @@
+import logging
 import brukva
 import tornado.httpserver
 import tornado.web
@@ -9,39 +10,39 @@ REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 
 r = redis.Redis(REDIS_HOST, REDIS_PORT, db=9)
-c = brukva.Client(REDIS_HOST, REDIS_PORT)
+c = brukva.Client(REDIS_HOST, REDIS_PORT, yield_mode=True, pool_size=10)
 c.connect()
 c.select(9)
 
-c.set('foo', 'bar')
-c.set('foo2', 'bar2')
+r.set('foo', 'bar')
+r.set('foo2', 'bar2')
 
 
 class BrukvaHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     @brukva.adisp.process
     def get(self):
-        foo, foo2 = yield [c.async.get('foo'), c.async.get('foo2')]
+        res = yield [c.get(key) for key in ['foo', 'foo2'] * 5]
         self.set_header('Content-Type', 'text/plain')
-        self.write(foo)
-        self.write(foo2)
+        for r_ in res:
+            self.write(r_)
         self.finish()
 
 
 class RedisHandler(tornado.web.RequestHandler):
     def get(self):
-        foo = r.get('foo')
-        foo2 = r.get('foo2')
+        res = [r.get(key) for key in ['foo', 'foo2'] * 5]
         self.set_header('Content-Type', 'text/plain')
-        self.write(foo)
-        self.write(foo2)
+        for r_ in res:
+            self.write(r_)
         self.finish()
 
 
 class HelloHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Content-Type', 'text/plain')
-        self.write('Hello world!')
+        for i in xrange(10):
+            self.write('Hello world!')
         self.finish()
 
 
@@ -49,7 +50,7 @@ application = tornado.web.Application([
     (r'/brukva', BrukvaHandler),
     (r'/redis', RedisHandler),
     (r'/hello', HelloHandler),
-])
+],debug=True)
 
 
 if __name__ == '__main__':
--- demos/simple/app.py
+++ demos/simple/app.py
@@ -8,16 +8,19 @@ import logging
 
 
 logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+formatter = logging.Formatter('%(msecs)s - %(filename)s:%(funcName)s:%(lineno)s - %(message)s')
+handler = logging.FileHandler('logs.txt')
+handler.setFormatter(formatter)
+logger.handlers[0].setFormatter(formatter)
+logger.addHandler(handler)
 
-log = logging.getLogger('app')
-
-
-c = brukva.Client()
+c = brukva.Client(yield_mode=True, reconnect_retries=2, reconnect_timeout=0.2, pool_size=4)
 c.connect()
 
 
 def on_set(result):
-    log.debug("set result: %s" % result)
+    logging.debug("set result: %s" % result)
 
 
 c.set('foo', 'Lorem ipsum #1', on_set)
@@ -29,17 +32,24 @@ class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     @adisp.process
     def get(self):
-        foo = yield c.async.get('foo')
-        bar = yield c.async.get('bar')
-        zar = yield c.async.get('zar')
-        self.set_header('Content-Type', 'text/html')
-        self.render("template.html", title="Simple demo", foo=foo, bar=bar, zar=zar)
+        try:
+            foo = yield c.get('foo')
+            bar = yield c.async.get('bar')
+            zar = yield c.async.get('zar')
+            self.set_header('Content-Type', 'text/html')
+            self.write('foo='+foo)
+            self.write('bar='+bar)
+            self.write('zar='+zar)
+            self.finish()
+        except Exception:
+            logger.exception('exception caught in handler')
+            self.write('exception happened')
+            self.finish()
 
 
 application = tornado.web.Application([
     (r'/', MainHandler),
-])
-
+], debug=True)
 
 if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(application)
